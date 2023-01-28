b4955592c77106c1fe6be27a8d3083e8b5f5f8a3
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-28 20:22:50 -0300

    Criando estrutura inicial

---
+++ EditorHTML/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
