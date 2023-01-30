5cbff11757061cb44c8bf8c5ee42e8933b5164e4
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-29 21:59:04 -0300

    Refatorando desenho de tela

---
+++ EditorHTML/DrawScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHTML
+{
+    public static class Screen
+    {
+        public static void Draw()
+        {
+            CreateLines();
+            CreateColumns();
+            CreateLines();
+        }
+
+        private static void CreateLines()
+        {
+            Console.Write("+");
+
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("+\n");
+        }
+
+        private static void CreateColumns()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("|\n");
+            }
+        }
+    }
+}
--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -14,42 +14,7 @@ namespace EditorHTML
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            DrawScreen();
-        }
-
-        public static void DrawScreen()
-        {
-            Console.Write("+");
-
-            for (int i = 0; i <= 30; i++)
-            {
-                Console.Write("-");
-            }
-
-            Console.Write("+");
-            Console.Write("\n");
-
-            for (int lines = 0; lines <= 10; lines++)
-            {
-                Console.Write("|");
-
-                for (int i = 0; i <= 30; i++)
-                {
-                    Console.Write(" ");
-                }
-
-                Console.Write("|");
-                Console.Write("\n");
-            }
-
-            Console.Write("+");
-
-            for (int i = 0; i <= 30; i++)
-            {
-                Console.Write("-");
-            }
-
-            Console.Write("+");
+            Screen.Draw();
         }
     }
 }
